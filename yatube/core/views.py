5862ac0805ad0d1b7c147a0ae2dcf173c3ad2f26
Сергей Висков <cocosi383@gmail.com>
2022-03-31 05:50:30 +0500

    2

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-def page_not_found(request, exception):
+def page_not_found(request):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
